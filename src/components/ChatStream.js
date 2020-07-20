7db986234cf3ecde44e00c32e96c9083ab33856a
johnnytortorici <johnny.tortorici@gmail.com>
2020-07-20 14:31:07 -0400

    Exercise 1

--- src/components/ChatStream.js
+++ src/components/ChatStream.js
@@ -11,7 +11,11 @@ import './ChatStream.css';
 // - a background color of #e9e9eb
 
 const ChatStream = (props) => {
-  return <section className='chat-stream'>{/* Your code here! */}</section>;
+  return <section className='chat-stream'>{
+    props.messages.map(message => {
+      return message.body;
+    })
+  }</section>;
 };
 
 export default ChatStream;
